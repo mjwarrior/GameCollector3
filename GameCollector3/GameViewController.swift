e1d90206b89640f43a562da60389686ff5dfe2e3
marcstreeter <mstreeter@gmail.com>
2017-01-03 16:23:50 -0600

    I have finished lecture 74 section 7  I have added a gauntlet image and I have made the buttons and the textfield and the image into code proceeding to the next lecture 77

---
+++ GameCollector3/GameViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GameViewController.swift
+//  GameCollector3
+//
+//  Created by Marc Streeter on 1/3/17.
+//  Copyright © 2017 Developer Warriors. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+   
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var gameImageView: UIImageView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+
+}
