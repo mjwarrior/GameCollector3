566b8a8a09871af390e2c75500946a670eaeb1c3
marcstreeter <mstreeter@gmail.com>
2017-01-05 12:12:37 -0600

    Just finished lecture 81 section 7 proceeding to lecture 82 now it can hide the delete button and we have update button but we need to fix it because it does not update it adds to the game controller tableview I will fix it

--- GameCollector3/GameViewController.swift
+++ GameCollector3/GameViewController.swift
@@ -11,17 +11,36 @@ import UIKit
 class GameViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
    
+    
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBOutlet weak var addupdatebutton: UIButton!
+    
     @IBOutlet weak var titleTextField: UITextField!
     
     @IBOutlet weak var gameImageView: UIImageView!
    
     var imagePicker = UIImagePickerController()
-
+    var game : Game? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
         imagePicker.delegate = self
-    }
+        
+        if game != nil {
+      gameImageView.image = UIImage(data: game!.image as! Data)
+     titleTextField.text = game!.title
+            
+    addupdatebutton.setTitle("Update", for: .normal)
+   
+        } else {
+         deleteButton.isHidden = true
+        }
+   
+       
+}
 
     @IBAction func photosTapped(_ sender: Any) {
     
